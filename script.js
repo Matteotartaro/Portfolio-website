85a35a8215dcae9c9b70a8dbf6b38c49b6de3598
Matteo <matteo@192.168.1.15>
2025-05-12 12:18:31 +0200

    Descrizione della modifica

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-function sendMail () {
+
+  function sendMail(event) {
+    event.preventDefault(); // impedisce il reload della pagina
+
     let parms = {
-        name: document.getElementById("name").value,
-        mail: document.getElementById("mail").value,
-        message: document.getElementById("message").value,
+      name: document.getElementById("name").value,
+      mail: document.getElementById("mail").value,
+      message: document.getElementById("message").value,
     };
 
     emailjs.send("service_7nljpjg", "template_fnoabjk", parms)
-    .then(function(response) {
+      .then(function (response) {
         alert("Email inviata!");
         console.log("SUCCESS", response);
-    }, function(error) {
+      }, function (error) {
         alert("Invio fallito.");
         console.error("FAILED", error);
-    });
-}
+      });
+  }
 
-document.getElementById("contact-form").addEventListener("submit", sendMail);
+  document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("contact-form").addEventListener("submit", sendMail);
+  });
